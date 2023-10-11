e214601778406ebaf2ac1a504bff8d2f2664b303
Kate Zinchenko <id.a1430@gmail.com>
2023-10-11 13:39:40 +0300

    entrypoint edit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-#poetry shell
-#poetry run django-admin
-#poetry config virtualenvs.create false
-#poetry install --no-root
-poetry shell
 poetry run python manage.py makemigrations
-#poetry run python manage.py makemigrations user_management
 poetry run python manage.py migrate
-#
-#poetry run python manage.py makemigrations admin
-#poetry run python manage.py makemigrations auth
-
 
 poetry run python manage.py runserver 0.0.0.0:8000
